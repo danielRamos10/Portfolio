0f60cf04fc25310cb4ae8183679b5b1bb3a19ebc
Ramos Posadas <959055@cognizant.com>
2022-10-31 13:51:32 -0500

    added snap scroll animation and cleaned up some styling

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,21 +1,21 @@
 // import logo from './logo.svg';
-import './App.css';
-import About from './Components/AboutMe/AboutMe';
-import Contact from './Components/Contact/Contact';
-import Landing from './Components/Landing/Landing';
-import Footer from './Components/Footer/Footer';
-import NavBar from './Components/NavBar/Navbar';
-import Skills from './Components/Skills/Skills';
+import "./App.css";
+import About from "./Components/AboutMe/AboutMe";
+import Contact from "./Components/Contact/Contact";
+import Landing from "./Components/Landing/Landing";
+import Footer from "./Components/Footer/Footer";
+import NavBar from "./Components/NavBar/Navbar";
+import Skills from "./Components/Skills/Skills";
 
 function App() {
   return (
-    <div className="app">
-      <NavBar/>
-      <Landing/>
-      <About/>
-      <Skills/>
-      <Contact/>
-      <Footer/>
+    <div className="component-container">
+      <NavBar />
+      <Landing />
+      <About />
+      <Skills />
+      <Contact />
+      <Footer />
     </div>
   );
 }
