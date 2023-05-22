d0fa12880afa6b3e8d3b166e38ab6807676a7ee9
Ramos Posadas <959055@cognizant.com>
2023-05-22 17:04:42 -0500

    navbar is almost done, still deciding on colors and placement

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,71 +1,41 @@
 
-import './navbar.css'
+import './mystyles.scss'
 import { useState } from 'react';
+import { Navbar } from 'react-bulma-components';
+import logo from '../assets/logo_dr_hr.png'
 function NavBar() {
 
-  const [toggle, setToggle]= useState(false);
+  const [toggle, setToggle] = useState(false);
   return (
-    
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-    <div className="navbar-brand">
-      <a className="navbar-item" href="https://bulma.io">
-        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
-      </a>
-  
-      <a role="button" className={`navbar-burger burger ${toggle ? "is-active" : ""}`} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" onClick={()=>{setToggle(!toggle)}}>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-  
-    <div id="navbarBasicExample" className={`navbar-menu ${toggle ? "is-active" : ""}`}>
-      <div className="navbar-start">
-        <a className="navbar-item">
-          Home
-        </a>
-  
-        <a className="navbar-item">
-          Documentation
-        </a>
-  
-        <div className="navbar-item has-dropdown is-hoverable">
-          <a className="navbar-link">
-            More
-          </a>
-  
-          <div className="navbar-dropdown">
-            <a className="navbar-item">
-              About
-            </a>
-            <a className="navbar-item">
-              Jobs
-            </a>
-            <a className="navbar-item">
-              Contact
-            </a>
-            <hr className="navbar-divider"/>
-            <a className="navbar-item">
-              Report an issue
-            </a>
-          </div>
-        </div>
-      </div>
-  
-      <div className="navbar-end">
-        <div className="navbar-item">
-          <div className="buttons">
-            <a className="button is-primary">
-              <strong>Sign up</strong>
-            </a>
-            <a className="button is-light">
-              Log in
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </nav>
+
+    <Navbar backgroundColor='blue-grey'>
+      <Navbar.Brand>
+        <Navbar.Item>
+          <img src={logo} width="100%" />
+        </Navbar.Item>
+
+        <Navbar.Burger className={` ${toggle ? "is-active" : ""}`} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" onClick={() => { setToggle(!toggle) }} backgroundColor='blue-grey'>
+        </Navbar.Burger>
+      </Navbar.Brand>
+
+      <Navbar.Menu className={`navbar-menu ${toggle ? "is-active" : ""}`} alignContent='center' backgroundColor='blue-grey'>
+        <Navbar.Container align='right' backgroundColor='blue-grey'>
+          <Navbar.Item >
+            Home
+          </Navbar.Item>
+          <Navbar.Item>
+            About
+          </Navbar.Item>
+          <Navbar.Item>
+            Skills
+          </Navbar.Item>
+          <Navbar.Item>
+            Contact
+          </Navbar.Item>
+        </Navbar.Container>
+
+      </Navbar.Menu>
+    </Navbar>
   );
 }
 
