dff2990c04ad2f94ec7c0e16f95cfec917c31328
Ramos Posadas <959055@cognizant.com>
2023-05-23 13:24:38 -0500

    clean the folder structure, added more component files, added resume btn to navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,25 +1,30 @@
 
 import './mystyles.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import logo from '../assets/log_svg.svg'
+import logo from '/src/assets/log_svg.svg'
 import { useState } from 'react';
-import { Navbar } from 'react-bulma-components';
+import { Navbar, Button } from 'react-bulma-components';
+
 import { faAddressCard, faHouse, faPhone, faLaptop } from '@fortawesome/free-solid-svg-icons';
 function NavBar() {
   const [toggle, setToggle] = useState(false);
   return (
-
     <Navbar backgroundColor='gunmetal'>
       <Navbar.Brand>
         <Navbar.Burger className={` ${toggle ? "is-active" : ""}`} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" onClick={() => { setToggle(!toggle) }} backgroundColor='gunmetal' id='navbarBurger'>
         </Navbar.Burger>
-        <img src={logo} alt="logo" className='nav-logo' />
+
+        <a href="/">
+          <img src={logo} alt="logo" className='nav-logo' />
+        </a>
+
+
       </Navbar.Brand>
 
       <Navbar.Menu className={`navbar-menu ${toggle ? "is-active" : ""}`} alignContent='center' backgroundColor='gunmetal'>
         <Navbar.Container align='right' backgroundColor='gunmetal'>
           <Navbar.Item>
-            <span><FontAwesomeIcon icon={faHouse}/></span>
+            <span><FontAwesomeIcon icon={faHouse} /></span>
             Home
           </Navbar.Item>
           <Navbar.Item>
@@ -33,6 +38,9 @@ function NavBar() {
           <Navbar.Item>
             <span> <FontAwesomeIcon icon={faPhone} /></span>
             <span> Contact</span>
+          </Navbar.Item>
+          <Navbar.Item renderAs='span'>
+            <Button color='bittersweet' textColor='lavender-blush' size='string' >Resume</Button>
           </Navbar.Item>
         </Navbar.Container>
       </Navbar.Menu>
