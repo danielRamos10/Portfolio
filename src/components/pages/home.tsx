7e1b16e347c20a036c85d7f816d89752aff0eb51
Daniel Ramos <danielramos@Daniels-MBP.lan>
2023-06-05 17:35:18 -0500

    image needs fixing

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,20 +1,26 @@
 import "./home.scss";
-import { Heading, Button } from "react-bulma-components";
+import { Heading, Button, Image } from "react-bulma-components";
+import me from "/src/assets/me.jpg";
 function Home() {
   return (
     <div className="home-container">
-      <div className="line">
+      <div className="line"></div>
+      <Heading className="intro">Hello, my name is</Heading>
+      <Heading className="name anim-typewriter">Daniel Ramos</Heading>
+      <Heading className="ocupation">Frontend Developer</Heading>
+      <Button
+        className="contact-btn"
+        color="flame"
+        textSize={6}
+        textWeight="bold"
+        textColor="flash-white"
+      >
+        {" "}
+        Let's Chat!
+      </Button>
+      <div className="img-container">
+        <img src={me} alt="me" className="my-image" height={200} width={200} />
       </div>
-      <Heading className="intro">
-        Hello, my name is
-      </Heading>
-      <Heading className="name anim-typewriter">
-      Daniel Ramos
-      </Heading>
-      <Heading className="ocupation">
-       Frontend Developer
-      </Heading>
-      <Button className="contact-btn" color='flame' textSize={6} textWeight="bold"> Let's Chat!</Button>
     </div>
   );
 }
