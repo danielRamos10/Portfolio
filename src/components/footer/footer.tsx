5300e436a2fe74a67172c457e0af852a78ffd193
Daniel Ramos <danielramos@Daniels-MBP.lan>
2023-06-04 21:34:32 -0500

    more styling

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,18 +1,32 @@
-import { Content} from "react-bulma-components";
-import './footer.scss'
-import ContentCard from "../content/card/content-card";
+import "./footer.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { Content, Heading } from "react-bulma-components";
 function FooterComponent() {
   return (
     <footer className="footer-container">
-      <ContentCard>
-        <Content textAlign={'center'} textColor="cyan" backgroundColor="gunmetal" justifyContent="center" flexDirection="column">
+      <div className="copyright">
+        <Heading size={6} textColor="cyan" alignContent="center">&copy; Copyright 2023 Daniel Ramos.</Heading>
+      </div>
+      <div className="brands">
+        <Content
+          alignItems="center"
+          justifyContent="center"
+          flexDirection="row"
+          textColor="cyan"
+        >
           <p>
-            &copy; Copyright 2023 Daniel Ramos.
+            <a href="https://www.linkedin.com/">LinkedIn</a>
+            <FontAwesomeIcon icon={faLinkedinIn} />
+          </p>
+          <p>
+            <a href="https://github.com/">GitHub</a>
+            <FontAwesomeIcon icon={faGithub} />
           </p>
         </Content>
-        </ContentCard>
+      </div>
     </footer>
-  )
+  );
 }
 
 export default FooterComponent;
