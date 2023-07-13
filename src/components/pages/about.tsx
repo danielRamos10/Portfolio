044a107f10a3c5b36e514d9f786b7f644bfcc324
Daniel Ramos <danielramos@Daniels-MBP.lan>
2023-07-13 11:40:18 -0500

    added images to about section

--- src/components/pages/about.tsx
+++ src/components/pages/about.tsx
@@ -3,14 +3,13 @@ import { AboutContent } from "../../data/content";
 import "./about.scss";
 import ContentCard from "../content/card/content-card";
 import ContentSection from "../content/section/content-section";
-
-
+import vaqueroslogo from "/src/assets/vaqueros.png";
+import fcb from '/src/assets/fcb.jpg';
 
 function About() {
- 
   return (
     <div id="aboutComponent" className="about-container">
-      <div className="about-grid-parent" >
+      <div className="about-grid-parent">
         <div className="headers">
           <Heading textColor="cyan">About</Heading>
           <Heading textColor="cyan" subtitle>
@@ -22,7 +21,8 @@ function About() {
           <ContentCard>
             <ContentSection
               title={AboutContent[0].title}
-              description={AboutContent[0].description} />
+              description={AboutContent[0].description}
+            />
           </ContentCard>
         </div>
         <div className="education">
@@ -33,6 +33,15 @@ function About() {
             />
           </ContentCard>
         </div>
+        <div className="vaqueros">
+          <img className="vaq-img" src={vaqueroslogo} alt="vaqueros" />
+        </div>
+        <div className="fcb">
+          <img className="img-1" src={fcb} alt="fcb" />
+          <img className="img-2" src={fcb} alt="fcb" />
+          <img className="img-3" src={fcb} alt="fcb" />
+          <img className="img-4" src={fcb} alt="fcb" />
+        </div>
         <div className="hobbies">
           <ContentCard>
             <ContentSection
@@ -41,12 +50,8 @@ function About() {
             />
           </ContentCard>
         </div>
-
-    </div>
-
+      </div>
     </div>
   );
 }
 export default About;
-
-
--- src/data/project-content.ts
+++ src/data/project-content.ts
@@ -3,19 +3,19 @@ export const ProjectContent = [
       title: "LeafyBuddy",
       image: "/src/assets/sand.jpg",
       description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo sunt ab quis vel magni veritatis, delectus accusantium labore quia fugit esse repellendus eaque quasi. Porro vel totam ullam? Doloribus, minima. Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
+        "LeafyBuddy is a single-page e-commerce Angular app that provides plant products, tools, and services. Users can create accounts to manage orders effectively. Utilizing AWS backend tools like Cognito, LeafyBuddy offers authentication, API handling, database storage, and a Twitter-like social feed for plant enthusiasts to share experiences and thoughts.",
     },
     {
       title: "RioVax",
       image: "/src/assets/sand.jpg",
       description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo sunt ab quis vel magni veritatis, delectus accusantium labore quia fugit esse repellendus eaque quasi. Porro vel totam ullam? Doloribus, minima. Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
+        "RioVax is a cross-platform Flutter app created to expedite the COVID-19 vaccination process. It provided up-to-date information and a user-friendly ListView of recommended hub locations based on geolocation, simplifying site selection for users.",
     },
     {
       title: "Pixelgram",
       image: "/src/assets/sand.jpg",
       description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo sunt ab quis vel magni veritatis, delectus accusantium labore quia fugit esse repellendus eaque quasi. Porro vel totam ullam? Doloribus, minima. Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
+        "Pixelgram is an Angular application reminiscent of Instagram, offering users a platform to share their pictures. The application integrates SpringBoot for all backend functionalities, including account creation, authentication, API endpoints, and database storage. This combination of frontend and backend technologies enables users to get the best experience. ",
     },
     {
       title: "JBook",
