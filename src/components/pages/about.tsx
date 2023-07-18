ecaed72146f560f33a0520ea34c5932684470f15
Daniel Ramos <danielramos@Daniels-MBP.lan>
2023-07-18 18:21:36 -0500

    animations

--- src/components/pages/about.tsx
+++ src/components/pages/about.tsx
@@ -5,50 +5,67 @@ import ContentCard from "../content/card/content-card";
 import ContentSection from "../content/section/content-section";
 import vaqueroslogo from "/src/assets/vaqueros.png";
 import fcb from "/src/assets/fcb.jpg";
+import Fade from "react-reveal/Fade";
+import Zoom from "react-reveal/Zoom";
+
 
 function About() {
+  
   return (
     <div id="aboutComponent" className="about-container">
       <div className="about-grid-parent">
-        <div className="headers">
-          <Heading textColor="cyan">About</Heading>
-          <Heading textColor="cyan" subtitle>
-            Me
-          </Heading>
-        </div>
+        <Fade left>
+          <div className="headers">
+            <Heading textColor="cyan">About</Heading>
+            <Heading textColor="cyan" subtitle>
+              Me
+            </Heading>
+          </div>
+        </Fade>
 
-        <div className="background">
-          <ContentCard>
-            <ContentSection
-              title={AboutContent[0].title}
-              description={AboutContent[0].description}
-            />
-          </ContentCard>
-        </div>
-        <div className="education">
-          <ContentCard>
-            <ContentSection
-              title={AboutContent[1].title}
-              description={AboutContent[1].description}
-            />
-          </ContentCard>
-        </div>
+        <Fade right>
+          <div className="background">
+            <ContentCard>
+              <ContentSection
+                title={AboutContent[0].title}
+                description={AboutContent[0].description}
+              />
+            </ContentCard>
+            Fade
+          </div>
+        </Fade>
+        <Fade left>
+          <div className="education">
+            <ContentCard>
+              <ContentSection
+                title={AboutContent[1].title}
+                description={AboutContent[1].description}
+              />
+            </ContentCard>
+          </div>
+        </Fade>
         <div className="vaqueros">
-          <img className="vaq-img" src={vaqueroslogo} alt="vaqueros" />
+          <Zoom>
+            <img className="vaq-img" src={vaqueroslogo} alt="vaqueros" />
+          </Zoom>
         </div>
-        <div className="fcb">
-          <img className="img-1" src={fcb} alt="fcb" />
-          <img className="img-2" src={fcb} alt="fcb" />
-          <img className="img-3" src={fcb} alt="fcb" />
-          <img className="img-4" src={fcb} alt="fcb" />
-        </div>
+        <Fade left>
+          <div className="fcb">
+            <img className="img-1" src={fcb} alt="fcb" />
+            <img className="img-2" src={fcb} alt="fcb" />
+            <img className="img-3" src={fcb} alt="fcb" />
+            <img className="img-4" src={fcb} alt="fcb" />
+          </div>
+        </Fade>
         <div className="hobbies">
-          <ContentCard>
-            <ContentSection
-              title={AboutContent[2].title}
-              description={AboutContent[2].description}
-            />
-          </ContentCard>
+          <Fade right>
+            <ContentCard>
+              <ContentSection
+                title={AboutContent[2].title}
+                description={AboutContent[2].description}
+              />
+            </ContentCard>
+          </Fade>
         </div>
       </div>
     </div>
